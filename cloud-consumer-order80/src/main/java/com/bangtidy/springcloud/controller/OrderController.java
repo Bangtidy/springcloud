3153bdb18fe8582594df2e14350e6f124419ed1f
bangtidy <bangtidy@qq.com>
2021-06-05 00:05:54 +0800

    搭建springcloud基础环境和Eureka基础框架

---
+++ cloud-consumer-order80/src/main/java/com/bangtidy/springcloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.bangtidy.springcloud.controller;
+
+import com.bangtidy.springcloud.entities.CommonResult;
+import com.bangtidy.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
+    //基于HTTP client调用远程服务
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+}
